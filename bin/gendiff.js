73ee9b55cbf1adf39654423fe2979452e0600a77
Radgab <radgab712@gmail.com>
2022-12-10 21:16:36 +0300

    add

---
+++ bin/gendiff.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+
+const program = new Command();
+
+program
+ .name('gendiff')
+ .arguments('<filepath1> <filepath2>')
+ .description('Compares two configuration files and shows a difference.')
+ .version('0.8.0')
+ .helpOption('-h, --help', 'output usage information')
+ .option('-f, --format <type>', 'output format')
+ 
+
+ program.parse()
