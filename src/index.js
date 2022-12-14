bb1093dd0f9e2aca36ef46812b057132c905968e
Radgab <radgab712@gmail.com>
2022-12-14 19:04:24 +0300

    add new file

---
+++ src/index.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+import fs from 'fs';// библиотека с помощью которой возможно работать с JSON
+import path from 'path';
+import treeСoincidences from './tree.js';
+
+
+function getPath(filepath) {
+    return (path.resolve(process.cwd(), filepath));// указывает абсолютный путь до
+  }
+  const readFilePath = (filepath) => fs.readFileSync((getPath(filepath)),'utf-8');
+  const gendiff = (filepath1, filepath2) => {
+   
+  const readFile = readFilePath(filepath1);//указываем путь где находится JSON файл
+  
+  const readFile2 = readFilePath(filepath2);
+  const parse = JSON.parse(readFile)
+  const parse2 = JSON.parse(readFile2)
+  const result = treeСoincidences(parse,parse2)
+  //console.log(result)
+  return result
+  }
+
+
+export {gendiff}
---
+++ src/tree.js
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+import _ from 'lodash';
+
+const treeСoincidences = (parse,parse2) => {
+   let file1 = _.cloneDeep(parse)
+   let file2 = _.cloneDeep(parse2)
+   let file3 = {}
+ 
+const mapTree = _.keys(file1).sort().map((key) => {
+
+if (_.has(file2, key) && file1[key] === file2[key] ) {
+
+    file3[key] =  file1[key] 
+
+    }else if (!_.has(file2, key) ) {
+
+        file3[` - ${key}`] = file1[key]
+
+    }  else if (_.has(file2, key) && file2[key] !== file1[key]){
+      
+      file3[` - ${key}`] = file1[key]
+      
+
+    }
+   
+})
+
+const mapTree2 = _.keys(file2).sort().map((key) => {
+ // console.log(file3)
+  if (!_.has(file3, key)) {
+    if (!_.has(file1, key) ) {
+          file3[` + ${key}`] = file2[key]
+
+      } else if (_.has(file1, key) && file1[key] !== file2[key]){
+        file3[` + ${key}`] = file2[key]
+
+      }
+
+  }
+
+  })
+
+
+
+ return file3
+
+}
+
+export default treeСoincidences;
