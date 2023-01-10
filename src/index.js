c9149095336fc68b39aa392702ba065c261b20ce
Radgab <radgab712@gmail.com>
2023-01-10 18:31:59 +0300

    fix src/index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 #!/usr/bin/env node
-import fs from 'fs';// библиотека с помощью которой возможно работать с JSON
-import path from 'path';
 import treeСoincidences from './tree.js';
+import readFilePath from '../parse1.js'
+import bulidTree from '/src/bulidTree.js'
 
-function getPath(filepath) {
-  return (path.resolve(process.cwd(), filepath));// указывает абсолютный путь до
-}
-const readFilePath = (filepath) => fs.readFileSync((getPath(filepath)), 'utf-8');
-
-//const readFilePathYaml = (filepath) => fs.readFileSync((getPath(filepath)),'utf-8');
 const gendiff = (filepath1, filepath2) => {
   const readFile = readFilePath(filepath1); //указываем путь где находится JSON файл
   const readFile2 = readFilePath(filepath2);
